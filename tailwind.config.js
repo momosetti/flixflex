a5de7b23aa2abee4d688650072f6102a8095f002
Momo Setti <zinasetti@gmail.com>
2023-01-28 23:59:25 +0100

    feat-dev: extend and configure Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#ffefef",
+          100: "#fedbdb",
+          200: "#febfbf",
+          300: "#fd9393",
+          400: "#fa6060",
+          500: "#f63b3b",
+          600: "#eb2525",
+          700: "#d81d1d",
+          800: "#af1e1e",
+          900: "#8a1e1e",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
