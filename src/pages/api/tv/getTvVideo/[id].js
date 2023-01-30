2411fff1ee143b92bdfad8a134644c95230236f4
Momo Setti <zinasetti@gmail.com>
2023-01-30 11:50:28 +0100

    feat: implement tv API

---
+++ src/pages/api/tv/getTvVideo/[id].js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { API_ENTRYPOINT } from "@/utils/constants";
+
+export default async function handler(req, res) {
+  const { id } = req.query;
+  const config = {
+    method: "get",
+    url: `${API_ENTRYPOINT}/tv/${id}/videos?api_key=${process.env.TMDB_API_KEY}`,
+  };
+
+  try {
+    let response = await axios(config);
+    res.status(200).json(response.data);
+  } catch (e) {
+    res.status(400).json(e.response);
+  }
+}
