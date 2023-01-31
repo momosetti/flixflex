857cab00e4357d70bacbb3b5e400e501bd70d404
Momo Setti <zinasetti@gmail.com>
2023-01-31 23:27:13 +0100

    feat: implement tv/movie API

---
+++ src/pages/api/tv/getTopTv.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const config = {
+    method: "get",
+    url: "https://api.themoviedb.org/3/tv/top_rated?api_key=8f52ff60fc237845607efe9791fdb6cb",
+  };
+
+  try {
+    let response = await axios(config);
+    res.status(200).json(response.data);
+  } catch (e) {
+    res.status(400).json(e.response);
+  }
+}
