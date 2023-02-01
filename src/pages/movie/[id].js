d9d75757f9f1aa8cd8dbd01b47520d9b6cd657cd
Momo Setti <zinasetti@gmail.com>
2023-02-01 03:30:23 +0100

    refactor: protect the user routes

--- src/pages/movie/[id].js
+++ src/pages/movie/[id].js
@@ -6,6 +6,7 @@ import ModalVideo from "react-modal-video";
 import { getFallbackImageLink, shimmer, toBase64 } from "@/utils/helpers";
 import Image from "next/image";
 import DocumentHead from "@/components/documentHead";
+import ProtectedRoute from "@/components/protectedRoute";
 
 export default function MovieDetail() {
   const [isOpen, setOpen] = useState(false);
@@ -47,38 +48,38 @@ export default function MovieDetail() {
   if (isLoading) {
     return (
       <main>
-        <div class="dank-ass-loader">
-          <div class="row">
-            <div class="arrow up outer outer-18"></div>
-            <div class="arrow down outer outer-17"></div>
-            <div class="arrow up outer outer-16"></div>
-            <div class="arrow down outer outer-15"></div>
-            <div class="arrow up outer outer-14"></div>
+        <div className="dank-ass-loader">
+          <div className="row">
+            <div className="arrow up outer outer-18"></div>
+            <div className="arrow down outer outer-17"></div>
+            <div className="arrow up outer outer-16"></div>
+            <div className="arrow down outer outer-15"></div>
+            <div className="arrow up outer outer-14"></div>
           </div>
-          <div class="row">
-            <div class="arrow up outer outer-1"></div>
-            <div class="arrow down outer outer-2"></div>
-            <div class="arrow up inner inner-6"></div>
-            <div class="arrow down inner inner-5"></div>
-            <div class="arrow up inner inner-4"></div>
-            <div class="arrow down outer outer-13"></div>
-            <div class="arrow up outer outer-12"></div>
+          <div className="row">
+            <div className="arrow up outer outer-1"></div>
+            <div className="arrow down outer outer-2"></div>
+            <div className="arrow up inner inner-6"></div>
+            <div className="arrow down inner inner-5"></div>
+            <div className="arrow up inner inner-4"></div>
+            <div className="arrow down outer outer-13"></div>
+            <div className="arrow up outer outer-12"></div>
           </div>
-          <div class="row">
-            <div class="arrow down outer outer-3"></div>
-            <div class="arrow up outer outer-4"></div>
-            <div class="arrow down inner inner-1"></div>
-            <div class="arrow up inner inner-2"></div>
-            <div class="arrow down inner inner-3"></div>
-            <div class="arrow up outer outer-11"></div>
-            <div class="arrow down outer outer-10"></div>
+          <div className="row">
+            <div className="arrow down outer outer-3"></div>
+            <div className="arrow up outer outer-4"></div>
+            <div className="arrow down inner inner-1"></div>
+            <div className="arrow up inner inner-2"></div>
+            <div className="arrow down inner inner-3"></div>
+            <div className="arrow up outer outer-11"></div>
+            <div className="arrow down outer outer-10"></div>
           </div>
-          <div class="row">
-            <div class="arrow down outer outer-5"></div>
-            <div class="arrow up outer outer-6"></div>
-            <div class="arrow down outer outer-7"></div>
-            <div class="arrow up outer outer-8"></div>
-            <div class="arrow down outer outer-9"></div>
+          <div className="row">
+            <div className="arrow down outer outer-5"></div>
+            <div className="arrow up outer outer-6"></div>
+            <div className="arrow down outer outer-7"></div>
+            <div className="arrow up outer outer-8"></div>
+            <div className="arrow down outer outer-9"></div>
           </div>
         </div>
       </main>
@@ -86,40 +87,26 @@ export default function MovieDetail() {
   }
   return (
     <>
-      <DocumentHead
-        title={isLoading ? "Loading ..." : movieDetail.original_title}
-      />
-      <ModalVideo
-        channel="youtube"
-        autoplay
-        isOpen={isOpen}
-        videoId={videoKeyRef.current}
-        onClose={() => setOpen(false)}
-      />
-      <NavBar />
-      <div className="container mx-auto py-16">
-        <section className="flex flex-col sm:mx-8 md:mx-0 md:flex-row md:items-start lg:justify-center">
-          <section className="px-20 text-center md:pr-8 md:pl-0 lg:w-2/5">
-            <span
-              style={{
-                boxSizing: "border-box",
-                display: "inline-block",
-                overflow: "hidden",
-                width: "initial",
-                height: "initial",
-                background: "none",
-                opacity: 1,
-                border: 0,
-                margin: 0,
-                padding: 0,
-                position: "relative",
-                maxWidth: "100%",
-              }}
-            >
+      <ProtectedRoute>
+        <DocumentHead
+          title={isLoading ? "Loading ..." : movieDetail.original_title}
+        />
+        <ModalVideo
+          channel="youtube"
+          autoplay
+          isOpen={isOpen}
+          videoId={videoKeyRef.current}
+          onClose={() => setOpen(false)}
+        />
+        <NavBar />
+        <div className="container mx-auto py-16">
+          <section className="flex flex-col sm:mx-8 md:mx-0 md:flex-row md:items-start lg:justify-center">
+            <section className="px-20 text-center md:pr-8 md:pl-0 lg:w-2/5">
               <span
                 style={{
                   boxSizing: "border-box",
-                  display: "block",
+                  display: "inline-block",
+                  overflow: "hidden",
                   width: "initial",
                   height: "initial",
                   background: "none",
@@ -127,16 +114,14 @@ export default function MovieDetail() {
                   border: 0,
                   margin: 0,
                   padding: 0,
+                  position: "relative",
                   maxWidth: "100%",
                 }}
               >
-                <img
-                  alt=""
-                  aria-hidden="true"
-                  src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27350%27%20height=%27530%27/%3e"
+                <span
                   style={{
+                    boxSizing: "border-box",
                     display: "block",
-                    maxWidth: "100%",
                     width: "initial",
                     height: "initial",
                     background: "none",
@@ -144,131 +129,132 @@ export default function MovieDetail() {
                     border: 0,
                     margin: 0,
                     padding: 0,
+                    maxWidth: "100%",
                   }}
+                >
+                  <img
+                    alt=""
+                    aria-hidden="true"
+                    src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27350%27%20height=%27530%27/%3e"
+                    style={{
+                      display: "block",
+                      maxWidth: "100%",
+                      width: "initial",
+                      height: "initial",
+                      background: "none",
+                      opacity: 1,
+                      border: 0,
+                      margin: 0,
+                      padding: 0,
+                    }}
+                  />
+                </span>
+                <Image
+                  alt={movieDetail.original_title}
+                  src={
+                    movieDetail.poster_path
+                      ? `${IMAGE_PATH}${movieDetail.poster_path}`
+                      : getFallbackImageLink(
+                          "853x1199",
+                          movieDetail.original_title || ""
+                        )
+                  }
+                  className="rounded-lg"
+                  style={{
+                    position: "absolute",
+                    inset: 0,
+                    boxSizing: "border-box",
+                    padding: 0,
+                    border: "none",
+                    margin: "auto",
+                    display: "block",
+                    width: 0,
+                    height: 0,
+                    minWidth: "100%",
+                    maxWidth: "100%",
+                    minHeight: "100%",
+                    maxHeight: "100%",
+                  }}
+                  width={600}
+                  height={450}
+                  placeholder="blur"
+                  blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                    shimmer(600, 450)
+                  )}`}
                 />
               </span>
-              <Image
-                alt={movieDetail.original_title}
-                src={
-                  movieDetail.poster_path
-                    ? `${IMAGE_PATH}${movieDetail.poster_path}`
-                    : getFallbackImageLink(
-                        "853x1199",
-                        movieDetail.original_title || ""
-                      )
-                }
-                className="rounded-lg"
-                style={{
-                  position: "absolute",
-                  inset: 0,
-                  boxSizing: "border-box",
-                  padding: 0,
-                  border: "none",
-                  margin: "auto",
-                  display: "block",
-                  width: 0,
-                  height: 0,
-                  minWidth: "100%",
-                  maxWidth: "100%",
-                  minHeight: "100%",
-                  maxHeight: "100%",
-                }}
-                width={600}
-                height={450}
-                placeholder="blur"
-                blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                  shimmer(600, 450)
-                )}`}
-              />
-            </span>
-          </section>
-          <section className="md:w-3/5">
-            <div className="mt-6 mb-2 text-center md:mt-0 md:mb-4 md:text-left">
-              <h1 className="mb-1 text-3xl font-bold md:mb-3 md:text-5xl">
-                {movieDetail.original_title}{" "}
-              </h1>
-              <p className="text-[17px] font-light">{movieDetail.tagline}</p>
-            </div>
-            <div className="mb-6 flex items-center justify-between text-left text-sm lg:w-10/12 lg:text-lg">
-              <div>
-                <p className="mb-1 text-app-placeholder">Length</p>
-                <p className="text-app-pure-white">
-                  {movieDetail.runtime} minutes.
-                </p>
+            </section>
+            <section className="md:w-3/5">
+              <div className="mt-6 mb-2 text-center md:mt-0 md:mb-4 md:text-left">
+                <h1 className="mb-1 text-3xl font-bold md:mb-3 md:text-5xl">
+                  {movieDetail.original_title}{" "}
+                </h1>
+                <p className="text-[17px] font-light">{movieDetail.tagline}</p>
               </div>
-              <div>
-                <p className="mb-1 text-app-placeholder">Language</p>
-                <p className="text-app-pure-white">{}</p>
+              <div className="mb-6 flex items-center justify-between text-left text-sm lg:w-10/12 lg:text-lg">
+                <div>
+                  <p className="mb-1 text-app-placeholder">Length</p>
+                  <p className="text-app-pure-white">
+                    {movieDetail.runtime} minutes.
+                  </p>
+                </div>
+                <div>
+                  <p className="mb-1 text-app-placeholder">Year</p>
+                  <p className="text-app-pure-white">
+                    {new Date(movieDetail.release_date).getFullYear()}
+                  </p>
+                </div>
+                <div>
+                  <p className="mb-1 text-app-placeholder">Status</p>
+                  <p className="text-app-pure-white">{movieDetail.status}</p>
+                </div>
               </div>
-              <div>
-                <p className="mb-1 text-app-placeholder">Year</p>
-                <p className="text-app-pure-white">
-                  {new Date(movieDetail.release_date).getFullYear()}
-                </p>
+              <div className="mb-6">
+                <h3 className="mb-2 md:text-lg">Genres</h3>
+                <ul className="flex flex-wrap text-xs font-light md:text-sm">
+                  {movieDetail.genres?.map((genre) => (
+                    <li
+                      key={genre.id}
+                      className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300"
+                    >
+                      {genre.name}
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <div>
-                <p className="mb-1 text-app-placeholder">Status</p>
-                <p className="text-app-pure-white">{movieDetail.status}</p>
+              <div className="mb-6">
+                <h3 className="mb-1 md:text-lg">Overview</h3>
+                <p className="font-light">{movieDetail.overview}</p>
               </div>
-            </div>
-            <div className="mb-6">
-              <h3 className="mb-2 md:text-lg">Genres</h3>
-              <ul className="flex flex-wrap text-xs font-light md:text-sm">
-                {movieDetail.genres?.map((genre) => (
-                  <li
-                    key={genre.id}
-                    className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300"
-                  >
-                    {genre.name}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="mb-6">
-              <h3 className="mb-1 md:text-lg">Overview</h3>
-              <p className="font-light">{movieDetail.overview}</p>
-            </div>
-            <div className="mb-10">
-              <h3 className="mb-2 md:text-lg">Casts</h3>
-              <ul className="flex flex-wrap text-xs md:text-sm">
-                <li class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
-                  Default
-                </li>
-                <li class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
-                  Default
-                </li>
-                <li class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">
-                  Default
-                </li>
-              </ul>
-            </div>
-            <div className="mb-10 flex flex-wrap">
-              <button
-                onClick={() => setOpen(true)}
-                type="button"
-                class="text-gray-900 bg-white hover:bg-primary-100 border border-primary-200 focus:ring-4 focus:outline-none focus:ring-primary-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-primary-800 dark:bg-white dark:border-primary-700 dark:text-primary-900 dark:hover:bg-primary-200 mr-2 mb-2"
-              >
-                <svg
-                  aria-hidden="true"
-                  class="w-8 h-4 mx-1 -ml-1"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="1.5"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+
+              <div className="mb-10 flex flex-wrap">
+                <button
+                  onClick={() => setOpen(true)}
+                  type="button"
+                  className="text-gray-900 bg-white hover:bg-primary-100 border border-primary-200 focus:ring-4 focus:outline-none focus:ring-primary-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-primary-800 dark:bg-white dark:border-primary-700 dark:text-primary-900 dark:hover:bg-primary-200 mr-2 mb-2"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-                  ></path>
-                </svg>
-                Trailer
-              </button>
-            </div>
+                  <svg
+                    aria-hidden="true"
+                    className="w-8 h-4 mx-1 -ml-1"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+                    ></path>
+                  </svg>
+                  Trailer
+                </button>
+              </div>
+            </section>
           </section>
-        </section>
-      </div>
+        </div>
+      </ProtectedRoute>
     </>
   );
 }
--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -2,14 +2,12 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import NavBar from "@/components/navBar";
-
+import ProtectedRoute from "@/components/protectedRoute";
 import axios from "axios";
 import DocumentHead from "@/components/documentHead";
 import dynamic from "next/dynamic";
 
-const DynamicShowCard = dynamic(() => import("@/components/showCard"), {
-  loading: () => "Loading...",
-});
+const DynamicShowCard = dynamic(() => import("@/components/showCard"));
 export default function SearchPage() {
   const [searchResult, setSearchResult] = useState(undefined);
   const router = useRouter();
@@ -17,98 +15,85 @@ export default function SearchPage() {
 
   useEffect(() => {
     async function fetchData() {
-      //   const data = await fetch(`/api/search?q=${q}&page=${page}`);
-      //   const res = await data.json();
-      //   const { results } = res;
-      //   setSearchResult(results);
-      //   console.log(res);
       const response = await axios.get(`/api/search?q=${q}&page=${page}`);
       setSearchResult(response.data);
-      console.log(response);
     }
     fetchData();
   }, [page, q]);
   return (
     <>
-      <DocumentHead title={`Search`} />
-      <NavBar />
-      <div class="container mx-auto mt-[2em]">
-        <div class="flex items-end">
-          <h1 class="text-[20px] font-bold py-px sm:py-0 text-light">
-            We find {searchResult?.total_results} results for your search
-          </h1>
-        </div>
-        <div className="grid grid-cols-4 gap-4 my-3">
-          {searchResult?.results?.map((show) =>
-            show.media_type !== "person" ? (
-              <DynamicShowCard
-                key={show.id}
-                movieData={show}
-                mediaType={show.media_type}
-              />
-            ) : null
-          )}
-        </div>
-        <div className="my-16 flex items-center justify-center">
-          <Link
-            href={
-              Number(page) > 1 ? `/search?q=${q}&page=${Number(page) - 1}` : "#"
-            }
-            class={`inline-flex ${
-              page === "1" ? "cursor-not-allowed" : null
-            } items-center px-4 py-2 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
-          >
-            <svg
-              aria-hidden="true"
-              class="w-5 h-5 mr-2"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+      <ProtectedRoute>
+        <DocumentHead title={`Search`} />
+        <NavBar />
+        <div className="container mx-auto mt-[2em]">
+          <div className="flex items-end">
+            <h1 className="text-[20px] font-bold py-px sm:py-0 text-light">
+              We find {searchResult?.total_results} results for your search
+            </h1>
+          </div>
+          <div classNameName="grid grid-cols-4 gap-4 my-3">
+            {searchResult?.results?.map((show) =>
+              show.media_type !== "person" ? (
+                <DynamicShowCard
+                  key={show.id}
+                  movieData={show}
+                  mediaType={show.media_type}
+                />
+              ) : null
+            )}
+          </div>
+          <div classNameName="my-16 flex items-center justify-center">
+            <Link
+              href={
+                Number(page) > 1
+                  ? `/search?q=${q}&page=${Number(page) - 1}`
+                  : "#"
+              }
+              className={`inline-flex ${
+                page === "1" ? "cursor-not-allowed" : null
+              } items-center px-4 py-2 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
             >
-              <path
-                fill-rule="evenodd"
-                d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            Previous
-          </Link>
-          <Link
-            href={
-              page < searchResult?.total_pages
-                ? `/search?q=${q}&page=${Number(page) + 1}`
-                : "#"
-            }
-            class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            Next
-            <svg
-              aria-hidden="true"
-              class="w-5 h-5 ml-2"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              Previous
+            </Link>
+            <Link
+              href={
+                page < searchResult?.total_pages
+                  ? `/search?q=${q}&page=${Number(page) + 1}`
+                  : "#"
+              }
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
-              <path
-                fill-rule="evenodd"
-                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </Link>
+              Next
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 ml-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </Link>
+          </div>
         </div>
-      </div>
+      </ProtectedRoute>
     </>
   );
-}
-export async function getServerSideProps({ req, res }) {
-  console.log(req.query);
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=10, stale-while-revalidate=59"
-  );
-
-  return {
-    props: {},
-  };
 }
