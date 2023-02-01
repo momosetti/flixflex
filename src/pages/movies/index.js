cee5eb77c82a7faed80fc629112d85298e5c6e31
Momo Setti <zinasetti@gmail.com>
2023-02-01 06:36:14 +0100

    refactor: embed the loader into the main node parent

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -22,47 +22,51 @@ export default function MoviesPage() {
     }
     fetchData();
   }, []);
-  if (isLoading) {
-    return <Loader />;
-  }
+
   return (
     <>
       <DocumentHead title={"Movies"} />
       <NavBar />
       <ProtectedRoute>
         <div class="container mx-auto py-16">
-          <div className="container py-5">
-            <div class="flex items-end">
-              <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
-                the 5 top movie
-              </h1>
-            </div>
-            <div className="grid grid-cols-3 gap-4 my-3">
-              {topMovies?.slice(0, 5).map((topMovie) => (
-                <ShowCard
-                  key={topMovie.id}
-                  movieData={topMovie}
-                  mediaType={"movie"}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="container py-5">
-            <div class="flex items-end">
-              <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
-                Movies
-              </h1>
-            </div>
-            <div className="grid grid-cols-5 gap-4 my-3">
-              {popularMovie?.map((topMovie) => (
-                <ShowCard
-                  key={topMovie.id}
-                  movieData={topMovie}
-                  mediaType={"movie"}
-                />
-              ))}
-            </div>
-          </div>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <div className="container py-5">
+                <div class="flex items-end">
+                  <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
+                    the 5 top movie
+                  </h1>
+                </div>
+                <div className="grid grid-cols-3 gap-4 my-3">
+                  {topMovies?.slice(0, 5).map((topMovie) => (
+                    <ShowCard
+                      key={topMovie.id}
+                      movieData={topMovie}
+                      mediaType={"movie"}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="container py-5">
+                <div class="flex items-end">
+                  <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
+                    Movies
+                  </h1>
+                </div>
+                <div className="grid grid-cols-5 gap-4 my-3">
+                  {popularMovie?.map((topMovie) => (
+                    <ShowCard
+                      key={topMovie.id}
+                      movieData={topMovie}
+                      mediaType={"movie"}
+                    />
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </ProtectedRoute>
     </>
--- src/pages/tv-shows/index.js
+++ src/pages/tv-shows/index.js
@@ -22,47 +22,51 @@ export default function TvPage() {
     }
     fetchData();
   }, []);
-  if (isLoading) {
-    return <Loader />;
-  }
+
   return (
     <>
       <DocumentHead title={"Tv Shows"} />
       <NavBar />
       <ProtectedRoute>
         <div class="container mx-auto py-16">
-          <div className="container py-5">
-            <div class="flex items-end">
-              <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
-                the 5 top TV shows
-              </h1>
-            </div>
-            <div className="grid grid-cols-3 gap-4 my-3">
-              {topMovies?.map((topMovie) => (
-                <ShowCard
-                  key={topMovie.id}
-                  movieData={topMovie}
-                  mediaType={"tv"}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="container py-5">
-            <div class="flex items-end">
-              <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
-                TV shows
-              </h1>
-            </div>
-            <div className="grid grid-cols-5 gap-4 my-3">
-              {popularTv?.map((topMovie) => (
-                <ShowCard
-                  key={topMovie.id}
-                  movieData={topMovie}
-                  mediaType={"tv"}
-                />
-              ))}
-            </div>
-          </div>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <div className="container py-5">
+                <div class="flex items-end">
+                  <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
+                    the 5 top TV shows
+                  </h1>
+                </div>
+                <div className="grid grid-cols-3 gap-4 my-3">
+                  {topMovies?.map((topMovie) => (
+                    <ShowCard
+                      key={topMovie.id}
+                      movieData={topMovie}
+                      mediaType={"tv"}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="container py-5">
+                <div class="flex items-end">
+                  <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
+                    TV shows
+                  </h1>
+                </div>
+                <div className="grid grid-cols-5 gap-4 my-3">
+                  {popularTv?.map((topMovie) => (
+                    <ShowCard
+                      key={topMovie.id}
+                      movieData={topMovie}
+                      mediaType={"tv"}
+                    />
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </ProtectedRoute>
     </>
