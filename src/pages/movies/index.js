8c854c8c1c523a0b22748c3abd6873fc72ac31fe
Momo Setti <zinasetti@gmail.com>
2023-02-01 05:28:12 +0100

    feat: add movies section

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -2,30 +2,52 @@ import { useEffect, useState } from "react";
 import NavBar from "@/components/navBar";
 import ShowCard from "@/components/showCard";
 import ProtectedRoute from "@/components/protectedRoute";
+import DocumentHead from "@/components/documentHead";
 export default function TopMoviesPage() {
   const [topMovies, setTopMovie] = useState(null);
+  const [popularMovie, setPopluarMovie] = useState(null);
   useEffect(() => {
     async function fetchData() {
       const data = await fetch("/api/movie/getTopMovies");
       const res = await data.json();
       const { results } = res;
-      setTopMovie(results.slice(0, 5));
+      setTopMovie(results);
+      const PopularMoviedata = await fetch("/api/movie/getPopularMovie");
+      const reponse = await PopularMoviedata.json();
+      setPopluarMovie(reponse.results.slice(0, 10));
     }
     fetchData();
   }, []);
   return (
     <>
+      <DocumentHead title={"Movies"} />
       <NavBar />
       <ProtectedRoute>
         <div class="container mx-auto py-16">
           <div className="container py-5">
             <div class="flex items-end">
-              <h1 class="text-[20px] font-bold py-px sm:py-0 uppercase">
-                the 5 top movies
+              <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
+                the 5 top movie
               </h1>
             </div>
-            <div className="grid grid-cols-4 gap-4 my-3">
-              {topMovies?.map((topMovie) => (
+            <div className="grid grid-cols-3 gap-4 my-3">
+              {topMovies?.slice(0, 5).map((topMovie) => (
+                <ShowCard
+                  key={topMovie.id}
+                  movieData={topMovie}
+                  mediaType={"movie"}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="container py-5">
+            <div class="flex items-end">
+              <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
+                Movies
+              </h1>
+            </div>
+            <div className="grid grid-cols-5 gap-4 my-3">
+              {popularMovie?.map((topMovie) => (
                 <ShowCard
                   key={topMovie.id}
                   movieData={topMovie}
