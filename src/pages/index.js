e19ee9b8b5ed008fedd6651b6d46f5f263621a78
Momo Setti <zinasetti@gmail.com>
2023-02-01 05:26:19 +0100

    refactor: omit the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import NavBar from "@/components/navBar";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/movies");
+  });
   return (
     <>
       <NavBar />
