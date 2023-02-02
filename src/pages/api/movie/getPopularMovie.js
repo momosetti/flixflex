919aa4cbb00a4e8319559994733f34c787900577
Momo Setti <zinasetti@gmail.com>
2023-02-02 09:46:07 +0100

    feat: handle page number query

--- src/pages/api/movie/getPopularMovie.js
+++ src/pages/api/movie/getPopularMovie.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { API_ENTRYPOINT } from "@/utils/constants";
 
 export default async function handler(req, res) {
+  const { page = "1" } = req.query;
   const config = {
     method: "get",
-    url: `${API_ENTRYPOINT}/movie/popular?api_key=${process.env.TMDB_API_KEY}`,
+    url: `${API_ENTRYPOINT}/movie/popular?api_key=${process.env.TMDB_API_KEY}&page=${page}`,
   };
 
   try {
--- src/pages/api/tv/getPopularTv.js
+++ src/pages/api/tv/getPopularTv.js
@@ -2,15 +2,16 @@ import axios from "axios";
 import { API_ENTRYPOINT } from "@/utils/constants";
 
 export default async function handler(req, res) {
+  const { page = "1" } = req.query;
   const config = {
     method: "get",
-    url: `${API_ENTRYPOINT}/tv/popular?api_key=${process.env.TMDB_API_KEY}`,
+    url: `${API_ENTRYPOINT}/tv/popular?api_key=${process.env.TMDB_API_KEY}&page=${page}`,
   };
 
   try {
     let response = await axios(config);
     res.status(200).json(response.data);
   } catch (e) {
-    res.status(400).json(e.response);
+    res.status(400).json(e.response.data);
   }
 }
