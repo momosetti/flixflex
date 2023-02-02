82a18f49eef5d052d948255abc7c213a6205b1da
Momo Setti <zinasetti@gmail.com>
2023-02-02 10:01:05 +0100

    feat: add movie and tv shows listing with batches

--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -1,13 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { useRouter } from "next/router";
 import NavBar from "@/components/navBar";
 import ShowCard from "@/components/showCard";
 import ProtectedRoute from "@/components/protectedRoute";
 import DocumentHead from "@/components/documentHead";
-import Loader from "@/components/loader";
+
 export default function MoviesPage() {
   const [topMovies, setTopMovie] = useState(null);
   const [popularMovie, setPopluarMovie] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [moviesData, setMoviesData] = useState(null);
+  const router = useRouter();
+  const { page = "1" } = router.query;
+  const isNextDataLoad = useRef(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -15,58 +19,121 @@ export default function MoviesPage() {
       const res = await data.json();
       const { results } = res;
       setTopMovie(results);
-      const PopularMoviedata = await fetch("/api/movie/getPopularMovie");
-      const reponse = await PopularMoviedata.json();
-      setIsLoading(false);
-      setPopluarMovie(reponse.results.slice(0, 10));
+      if (isNextDataLoad.current) {
+        isNextDataLoad.current = false;
+        const popularMovieData = await fetch(
+          `/api/movie/getPopularMovie?page=${
+            Number(page) - 1 >= 1 ? Number(page) - 1 : 1
+          }`
+        );
+        const response = await popularMovieData.json();
+        setMoviesData(response.results);
+        setPopluarMovie(response.results.slice(0, 10));
+      } else {
+        setPopluarMovie(moviesData?.slice(10, 20));
+        isNextDataLoad.current = true;
+      }
     }
     fetchData();
-  }, []);
+  }, [page]);
 
   return (
     <>
-      <DocumentHead title={"Movies"} />
+      <DocumentHead title="Movies" />
       <NavBar />
       <ProtectedRoute>
         <div class="container mx-auto py-16">
-          {isLoading ? (
-            <Loader />
-          ) : (
-            <>
-              <div className="container py-5">
-                <div class="flex items-end">
-                  <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
-                    the 5 top movie
-                  </h1>
-                </div>
-                <div className="grid grid-cols-3 gap-4 my-3">
-                  {topMovies?.slice(0, 5).map((topMovie) => (
-                    <ShowCard
-                      key={topMovie.id}
-                      movieData={topMovie}
-                      mediaType={"movie"}
-                    />
-                  ))}
-                </div>
+          <>
+            <div className="container py-5">
+              <div class="flex items-end">
+                <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
+                  the 5 top movie
+                </h1>
               </div>
-              <div className="container py-5">
-                <div class="flex items-end">
-                  <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
-                    Movies
-                  </h1>
-                </div>
-                <div className="grid grid-cols-5 gap-4 my-3">
-                  {popularMovie?.map((topMovie) => (
-                    <ShowCard
-                      key={topMovie.id}
-                      movieData={topMovie}
-                      mediaType={"movie"}
-                    />
-                  ))}
-                </div>
+              <div className="grid grid-cols-5 gap-4 my-3">
+                {topMovies?.slice(0, 5).map((topMovie) => (
+                  <ShowCard
+                    key={topMovie.id}
+                    movieData={topMovie}
+                    mediaType={"movie"}
+                  />
+                ))}
               </div>
-            </>
-          )}
+            </div>
+            <div id="movies" className="container py-5">
+              <div class="flex items-end">
+                <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
+                  Movies
+                </h1>
+              </div>
+              <div className="grid grid-cols-3 gap-4 my-3">
+                {popularMovie?.map((topMovie) => (
+                  <ShowCard
+                    key={topMovie.id}
+                    movieData={topMovie}
+                    mediaType={"movie"}
+                  />
+                ))}
+              </div>
+              <div className="my-16 flex items-center justify-center">
+                <button
+                  onClick={() => {
+                    if (Number(page) - 1 === 0) return;
+                    router.push(`/movies?page=${Number(page) - 1}`, undefined, {
+                      scroll: false,
+                    });
+                    document.getElementById("movies").scrollIntoView();
+                  }}
+                  className={`${
+                    page === "1" ? "cursor-not-allowed" : null
+                  } inline-flex items-center px-4 py-2 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-primary-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 mr-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  Previous
+                </button>
+                <button
+                  onClick={() => {
+                    router.push(
+                      `/movies?page=${
+                        Number(page) + 1 > 999 ? 1 : Number(page) + 1
+                      }`, // the API limit the page number to 500
+                      undefined,
+                      { scroll: false }
+                    );
+                    document.getElementById("movies").scrollIntoView();
+                  }}
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-primary-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Next
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 ml-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </>
         </div>
       </ProtectedRoute>
     </>
--- src/pages/tv-shows/index.js
+++ src/pages/tv-shows/index.js
@@ -1,31 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { useRouter } from "next/router";
 import NavBar from "@/components/navBar";
 import ShowCard from "@/components/showCard";
 import ProtectedRoute from "@/components/protectedRoute";
 import DocumentHead from "@/components/documentHead";
 import Loader from "@/components/loader";
 export default function TvPage() {
-  const [topMovies, setTopMovie] = useState(null);
+  const [topTvShows, setTopTvShows] = useState(null);
+  const [tvShowsData, setTvShowsData] = useState(null);
   const [popularTv, setPopluarTv] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  const { page = "1" } = router.query;
+  const isNextDataLoad = useRef(true);
 
   useEffect(() => {
     async function fetchData() {
       const data = await fetch("/api/tv/getTopTv");
       const res = await data.json();
       const { results } = res;
-      setTopMovie(results.slice(0, 5));
-      const PopularTvdata = await fetch("/api/tv/getPopularTv");
-      const reponse = await PopularTvdata.json();
-      setIsLoading(false);
-      setPopluarTv(reponse.results.slice(0, 10));
+      setTopTvShows(results.slice(0, 5));
+      if (isNextDataLoad.current) {
+        isNextDataLoad.current = false;
+        const popularTvdata = await fetch(
+          `/api/tv/getPopularTv?page=${
+            Number(page) - 1 >= 1 ? Number(page) - 1 : 1
+          }`
+        );
+        const response = await popularTvdata.json();
+        setTvShowsData(response.results);
+        setPopluarTv(response.results.slice(0, 10));
+      } else {
+        setPopluarTv(tvShowsData?.slice(10, 20));
+        isNextDataLoad.current = true;
+      }
     }
     fetchData();
-  }, []);
+  }, [page]);
 
   return (
     <>
-      <DocumentHead title={"Tv Shows"} />
+      <DocumentHead title="Tv Shows" />
       <NavBar />
       <ProtectedRoute>
         <div class="container mx-auto py-16">
@@ -39,8 +53,8 @@ export default function TvPage() {
                     the 5 top TV shows
                   </h1>
                 </div>
-                <div className="grid grid-cols-3 gap-4 my-3">
-                  {topMovies?.map((topMovie) => (
+                <div className="grid grid-cols-5 gap-4 my-3">
+                  {topTvShows?.map((topMovie) => (
                     <ShowCard
                       key={topMovie.id}
                       movieData={topMovie}
@@ -49,13 +63,13 @@ export default function TvPage() {
                   ))}
                 </div>
               </div>
-              <div className="container py-5">
+              <div id="tv-shows" className="container py-5">
                 <div class="flex items-end">
                   <h1 class="text-[35px] font-light py-px sm:py-0 uppercase">
                     TV shows
                   </h1>
                 </div>
-                <div className="grid grid-cols-5 gap-4 my-3">
+                <div className="grid grid-cols-3 gap-4 my-3">
                   {popularTv?.map((topMovie) => (
                     <ShowCard
                       key={topMovie.id}
@@ -63,6 +77,65 @@ export default function TvPage() {
                       mediaType={"tv"}
                     />
                   ))}
+                </div>
+                <div className="my-16 flex items-center justify-center">
+                  <button
+                    onClick={() => {
+                      if (Number(page) - 1 === 0) return;
+                      router.push(
+                        `/tv-shows?page=${Number(page) - 1}`,
+                        undefined,
+                        { scroll: false }
+                      );
+                      document.getElementById("tv-shows").scrollIntoView();
+                    }}
+                    className={`${
+                      page === "1" ? "cursor-not-allowed" : null
+                    } inline-flex items-center px-4 py-2 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-primary-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+                  >
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 mr-2"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Previous
+                  </button>
+                  <button
+                    onClick={() => {
+                      router.push(
+                        `/tv-shows?page=${
+                          Number(page) + 1 > 999 ? 1 : Number(page) + 1
+                        }`, // the API limit the page number to 500
+                        undefined,
+                        { scroll: false }
+                      );
+                      document.getElementById("tv-shows").scrollIntoView();
+                    }}
+                    className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-primary-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Next
+                    <svg
+                      aria-hidden="true"
+                      className="w-5 h-5 ml-2"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
                 </div>
               </div>
             </>
